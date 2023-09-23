0cdec0c8dfad3293d9da46694650cfaf67ed7973
salembsaleh <salembsaleh@gmail.com>
2023-09-22 21:44:30 -0500

    Created dashboard and added TodoList Widget

--- HackRice-TodoList/src/App.jsx
+++ HackRice-TodoList/src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
 import "./App.css";
+import TodoList from "./components/TodoList";
+
 function App() {
+  const [widgets, setWidgets] = useState([]);
+
+  const addTodoList = () => {
+    setWidgets([
+      ...widgets,
+      <TodoList key={Date.now()} removeWidget={removeWidget} />,
+    ]);
+  };
+
+  const removeWidget = (widgetIndex) => {
+    const updatedWidgets = widgets.filter((_, index) => index !== widgetIndex);
+    setWidgets(updatedWidgets);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Personalized Productivity Dashboard</h1>
       </header>
-      <div className="Dashboard">
-        {/* Widget components will go here */}
-        <Widget title="To-Do List" />
-        <Widget title="Calendar" />
-        {/* Add more widgets as needed */}
+      <div className="Widgets">
+        <div className="WidgetSelector">
+          <h2>Available Widgets</h2>
+          <button onClick={addTodoList}>Add To-Do List</button>
+          {/* Add buttons for other widgets here */}
+        </div>
+        <div className="Dashboard">
+          {widgets.map((widget, index) => (
+            <div className="WidgetContainer" key={index}>
+              {widget}
+              <button onClick={() => removeWidget(index)}>
+                Remove TodoList
+              </button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- HackRice-TodoList/src/components/TodoList.jsx
+++ HackRice-TodoList/src/components/TodoList.jsx
@@ -1,11 +1,41 @@
-import React from "react";
-import "./TodoList.css";
+import React, { useState } from "react";
 
 function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
+
+  const handleRemoveTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="Widget">
       <h2>To-Do List</h2>
-      {/* Add To-Do List functionality here */}
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Add a new task"
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleRemoveTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
