3a015679e66ebb142e8dd8726994b028236a73dc
salembsaleh <salembsaleh@gmail.com>
2023-09-23 21:26:13 -0500

    Added weather widget
    Still need to make it not glitch when entering the name

---
+++ HackRice-TodoList/src/components/WeatherWidget.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherWidget.css";
+
+function WeatherWidget() {
+  const [city, setCity] = useState("New York"); // Default city
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    let ignore = false;
+
+    const fetchWeatherData = async () => {
+      setLoading(true);
+
+      try {
+        const apiKey = "996d9fca3a863edf72160d5287e0a8b2";
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        );
+
+        if (!response.ok) {
+          console.error("Error fetching weather data:", response.statusText);
+          setLoading(false);
+          return;
+        }
+
+        const data = await response.json();
+
+        if (!ignore) {
+          setWeatherData(data);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        if (!ignore) {
+          setLoading(false);
+        }
+      }
+    };
+
+    if (city.trim() !== "") {
+      fetchWeatherData();
+    } else {
+      setLoading(false);
+    }
+
+    return () => {
+      ignore = true;
+    };
+  }, [city]);
+
+  return (
+    <div className="Widget">
+      <h2>Weather Widget</h2>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city"
+      />
+      {loading && <div>Loading...</div>}
+      {!loading && weatherData && (
+        <div>
+          <p>City: {weatherData.name}</p>
+          <p>
+            Temperature:{" "}
+            {(((weatherData.main.temp - 273.15) * 9) / 5 + 32).toFixed(2)} °F
+          </p>
+          <p>
+            Description:{" "}
+            {weatherData.weather[0].description.charAt(0).toUpperCase() +
+              weatherData.weather[0].description.slice(1)}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default WeatherWidget;
